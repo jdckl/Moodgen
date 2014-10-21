dae5fca484066debe6f2b4a5cc79d4b785004b2d
violetzie <honza.doc@gmail.com>
2014-10-22 00:05:44 +0200

    Further changes..
    it still doesn’t work -.- :D

--- js/flckr.js
+++ js/flckr.js
@@ -1,4 +1,4 @@
-var searchTerm = $("#searchbx").val();
+var searchTerm = $("#sbx").val();
 var Flickurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=376b144109ffe90065a254606c9aae3d&";
 var tags = "&tags=" + searchTerm;
 var tagmode = "&tagmode=any";
@@ -6,13 +6,9 @@ var jsonFormat = "&format=json";
 var FinalURL = Flickurl + tags + tagmode + jsonFormat;
 
 $(document).ready(function() {
-	$("#srch").submit(function() {
-		$.getJSON( FinalURL, function(data){
-			
-
-			
-        });
-		})
-
-	});
-});
+      $("#btn").click(function(event){
+          $.getJSON('FinalURL', function(photo) {
+             $('#content').append('<img src="' + 'https://www.flickr.com/photos/' + photo.owner + '/' +  photo.id + '"/>');
+          });
+      });
+   });
