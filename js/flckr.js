514358674568935eb167c597479f1058ed1b8785
violetzie <honza.doc@gmail.com>
2014-10-22 16:01:37 +0200

    Yay
    It partially works now :D

--- js/flckr.js
+++ js/flckr.js
@@ -1,14 +1,19 @@
-var searchTerm = $("#sbx").val();
-var Flickurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=376b144109ffe90065a254606c9aae3d&";
-var tags = "&tags=" + searchTerm;
-var tagmode = "&tagmode=any";
-var jsonFormat = "&format=json";
-var FinalURL = Flickurl + tags + tagmode + jsonFormat;
+$(document).ready(function () {
+    $("#btn").click(function (event) {
+        var searchTerm = $("#sbx").val();
+        var Flickurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=376b144109ffe90065a254606c9aae3d&";
+        var tags = "&tags=" + searchTerm;
+        var tagmode = "&tagmode=any";
+        var jsonFormat = "&format=json&nojsoncallback=1";
+        var FinalURL = Flickurl + tags + tagmode + jsonFormat;
 
-$(document).ready(function() {
-      $("#btn").click(function(event){
-          $.getJSON('FinalURL', function(photo) {
-             $('#content').append('<img src="' + 'https://www.flickr.com/photos/' + photo.owner + '/' +  photo.id + '"/>');
+         $.getJSON(FinalURL, function(photos) {
+             var photo = photos.photos.photo[0];
+             console.log(photo);
+             
+             var photoUrl = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
+             
+             $('#content').append('<img src="' + photoUrl + '" class="pic"/>');
           });
-      });
-   });
+    });
+});
