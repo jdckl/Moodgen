f02a7e36df9bc8bb67974f870e3daa4dc0735176
violetzie <honza.doc@gmail.com>
2014-10-22 17:29:35 +0200

    100% Working fetch
    + masonry (delay so far …)

--- js/flckr.js
+++ js/flckr.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $("#btn").click(function (event) {
+
         var searchTerm = $("#sbx").val();
         var Flickurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=376b144109ffe90065a254606c9aae3d&";
         var tags = "&tags=" + searchTerm;
@@ -8,12 +9,23 @@ $(document).ready(function () {
         var FinalURL = Flickurl + tags + tagmode + jsonFormat;
 
          $.getJSON(FinalURL, function(photos) {
-             var photo = photos.photos.photo[0];
+             var photo = photos.photos.photo;
              console.log(photo);
-             
-             var photoUrl = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
-             
-             $('#content').append('<img src="' + photoUrl + '" class="pic"/>');
+                                   
+             $.each(photo, function(i, item) {
+             $('#content').append('<img src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic"/>');
+             });
+
+
+             setTimeout(function() {
+        var $container = $('#content');
+			$container.isotope({
+  			itemSelector: '.pic',
+  			layoutMode: 'masonry',
+  			containerStyle: null
+		});
+		}, 6000);
+
           });
     });
 });
