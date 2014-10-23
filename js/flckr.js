36343d51a1d792d77403c455890f99343c5d4a3f
violetzie <honza.doc@gmail.com>
2014-10-23 20:54:13 +0200

    big changes
    :3

--- js/flckr.js
+++ js/flckr.js
@@ -9,20 +9,73 @@ $(document).ready(function () {
         var tagmode = "&tagmode=any";
         var jsonFormat = "&format=json&nojsoncallback=1";
         var FinalURL = Flickurl + tags + tagmode + jsonFormat;
+        var keepers = [];
 
          $.getJSON(FinalURL, function(photos) {
              var photo = photos.photos.photo;
              console.log(photo);
+
+             $('#content').load(function() {
+             
+                console.log("load");
+
+            });
+
+             var doneNumber = 0;
                                    
              $.each(photo, function(i, item) {
-             $('#content').append('<div id="ptrn"><img src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic"></img></div>');
+             
+                 $('#content').append('<img id="' + item.id + '" src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic" />');
+
+                 $('#' + item.id).load(function() {
+             
+                    doneNumber = doneNumber + 1;
+
+                    console.log(doneNumber);
+
+                     if (doneNumber == photo.length) {
+
+                        console.log("all loaded");
+
+                        $("#loading").fadeOut('slow');
+
+                    }
+
+                });
+             
              });
 
-             $("#loading").fadeIn('slow').delay( 2000 ).fadeOut('slow');
+             $("#loading").fadeIn('slow');
 
-             $(".pic").click(function(){
-    			$(this).toggleClass("selected");
-			});
+            $(".pic").click(function(){
+    	    
+               $(this).toggleClass("selected");
+
+               
+            
+               if ( $( ".pic" ).is( ".selected" ) ) {
+            
+                    $("#info").fadeIn("fast");
+
+                    keepers.push($(this).attr('src'));
+            
+                } else {
+
+                    $("#info").fadeOut("fast");
+
+                    keepers.push($(this).attr('src'));
+                
+                }
+
+                console.log(keepers);
+	       
+            });
+
+            $("#btn3").click(function(){
+                $(".pic").removeClass("selected");
+                $("#info").fadeOut("fast");
+            });
+
           });
     });
 });
