1eb17729edacdb776d46b38a3328228484024f6e
violetzie <honza.doc@gmail.com>
2014-10-24 14:50:49 +0200

    Cosmetic changes ;D
    Awesome :3

--- js/flckr.js
+++ js/flckr.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     $("#btn").click(function (event) {
 
+        if ( $( "#header" ).is( ".focused" ) ) { 
+                } else {
+        $("#header").addClass("focused");                
+                }
+
     	$('#content').empty();
 
         var searchTerm = $("#sbx").val();
@@ -8,21 +13,77 @@ $(document).ready(function () {
         var tags = "&tags=" + searchTerm;
         var tagmode = "&tagmode=any";
         var jsonFormat = "&format=json&nojsoncallback=1";
-        var FinalURL = Flickurl + tags + tagmode + jsonFormat;
+        var limit = "&per_page=250";
+        var FinalURL = Flickurl + tags + tagmode + limit + jsonFormat;
+        var keepers = [];
 
          $.getJSON(FinalURL, function(photos) {
              var photo = photos.photos.photo;
              console.log(photo);
+
+             $('#content').load(function() {
+             
+                console.log("load");
+
+            });
+
+             var doneNumber = 0;
                                    
              $.each(photo, function(i, item) {
-             $('#content').append('<div id="ptrn"><img src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic"></img></div>');
+             
+                 $('#content').append('<img id="' + item.id + '" src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic" />');
+
+                 $('#' + item.id).load(function() {
+             
+                    doneNumber = doneNumber + 1;
+
+                    console.log(doneNumber);
+
+                     if (doneNumber == photo.length) {
+
+                        console.log("all loaded");
+
+                        $("#loading").fadeOut('slow');
+
+                    }
+
+                });
+             
              });
 
-             $("#loading").fadeIn('slow').delay( 2000 ).fadeOut('slow');
+             $("#loading").fadeIn('slow');
 
-             $(".pic").click(function(){
-    			$(this).toggleClass("selected");
-			});
+            $(".pic").click(function(){
+    	    
+               $(this).toggleClass("selected");
+
+               
+            
+               if ( $( ".pic" ).is( ".selected" ) ) {
+            
+                    $("#info").fadeIn("fast");
+
+                    keepers.push($(this).attr('src'));
+            
+                } else {
+
+                    $("#info").fadeOut("fast");
+
+                    keepers.push($(this).attr('src'));
+                
+                }
+
+                console.log(keepers);
+	       
+            });
+
+            $("#btn3").click(function(){
+                $(".pic").removeClass("selected");
+                $("#info").fadeOut("fast");
+            });
+
           });
     });
 });
+
+
