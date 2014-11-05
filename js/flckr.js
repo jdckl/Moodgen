4b58248b840ae63b30edae7df971d09c46032940
violetzie <honza.doc@gmail.com>
2014-11-05 21:04:25 +0100

    Making the DONE and MORE Processes
    Yup!

--- js/flckr.js
+++ js/flckr.js
@@ -13,9 +13,8 @@ $(document).ready(function () {
         var tags = "&tags=" + searchTerm;
         var tagmode = "&tagmode=any";
         var jsonFormat = "&format=json&nojsoncallback=1";
-        var limit = "&per_page=250";
+        var limit = "&per_page=100";
         var FinalURL = Flickurl + tags + tagmode + limit + jsonFormat;
-		var FinalURL = Flickurl + tags + tagmode + jsonFormat;
         var keepers = [];
 
          $.getJSON(FinalURL, function(photos) {
@@ -44,7 +43,8 @@ $(document).ready(function () {
                         $("#loading").fadeOut('slow');
                         $("#sbx").val('');
                         $("#sbx").attr("placeholder", "Woohoo I'm fast! :D");
-                    }
+
+                        }
                 });
              });
 
@@ -79,8 +79,66 @@ $(document).ready(function () {
                 console.log(keepers);
             });
 
+
+            // The DONE process! ****************
             $("#btn2").click(function(){
                 $('#content').empty();
+                 _.forEach( keepers , function(url) { 
+                    console.log(url);
+                    $('#content').append('<img class="pic" src="'+ url +'" />');
+                });
+
+            });
+
+
+            //Search more tags process!**************
+            $("#btnmore").click(function(){
+
+                $('#content').empty();
+                _.forEach( keepers , function(url) { 
+                    console.log(url);
+                    $('#content').append('<img id="' + url + '" class="pic selected" src="'+ url +'" />');
+
+        var searchTerm = $("#sbx2").val();
+        var Flickurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=376b144109ffe90065a254606c9aae3d&";
+        var tags = "&tags=" + searchTerm;
+        var tagmode = "&tagmode=any";
+        var jsonFormat = "&format=json&nojsoncallback=1";
+        var limit = "&per_page=100";
+        var FinalURL = Flickurl + tags + tagmode + limit + jsonFormat;
+
+        $.getJSON(FinalURL, function(photos) {
+             var photo = photos.photos.photo;
+             console.log(photo);
+
+             $('#content').load(function() {
+             
+                console.log("load");
+
+            });
+
+             var doneNumber = 0;
+
+            $.each(photo, function(i, item) {
+             
+                 $('#content').append('<img id="' + item.id + '" src="' + "https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" +item.id + "_" + item.secret + ".jpg"+ '" class="pic" />');
+                 $('#' + item.id).load(function() {
+             
+                    doneNumber = doneNumber + 1;
+
+                     if (doneNumber == photo.length) {
+
+                        $("#loading").fadeOut('slow');
+                        $("#sbx2").val('');
+                        $("#sbx2").attr("placeholder", "Yay! :D");
+
+                        }
+                });
+             });
+        }); //JSON end
+$("#loading").fadeIn('slow');
+
+                });
 
             });
            
